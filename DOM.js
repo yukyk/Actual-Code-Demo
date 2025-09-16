64a10ccd8502b02446cef7942a44178cb034b2e6
yukyk <yusufkhambaty1@gmail.com>
2025-09-16 22:09:09 +0530

    getElementsById

---
+++ DOM.js
@@ -0,0 +1,15 @@
+const mainH = document.getElementById('main-heading');
+mainH.textContent = "Fruit World";
+
+mainH.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.background = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const text = document.getElementById('basket-heading');
+
+text.style.color = 'green';
+
+const thank = document.getElementById('thanks');
+thank.innerHTML = '<p>Please visit us again</p>'
