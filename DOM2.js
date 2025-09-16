691a587b8f384e7fe562b9ddfe3f9199144f41a0
yukyk <yusufkhambaty1@gmail.com>
2025-09-16 22:06:57 +0530

    getElementsByClassName

---
+++ DOM2.js
@@ -0,0 +1,5 @@
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow'
+for (var i = 0; i < fruit.length; i++){
+    fruit[i].style.fontWeight = 'bold';
+}
