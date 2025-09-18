72399b5c60359f4cc7e36c45f0a7646b6901d339
yukyk <yusufkhambaty1@gmail.com>
2025-09-18 10:38:56 +0530

    Pesudo Classes and Elements

---
+++ DOM7.js
@@ -0,0 +1,25 @@
+button:hover{
+    background-color: lightblue;
+}
+
+
+input:focus{
+    border: 2px solid red;
+}
+
+
+
+.container::first-letter{
+    font-size: 2em;
+    font-weight: bold;
+}
+
+.container::first-line{
+    font-weight: bold;
+}
+
+container::selection{
+    background-color: yellow;
+    color: black;
+}
+
