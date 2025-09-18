fa9c8325ca949d9492c6fd05dd06732e87d55ed7
yukyk <yusufkhambaty1@gmail.com>
2025-09-18 21:03:55 +0530

    CSS Grid Code

---
+++ DOM10.js
@@ -0,0 +1,19 @@
+.grid-container{
+    display: grid;
+    grid-template-columns: repeat(3,100px);
+    grid-template-rows: repeat(3,100px);
+    gap:10px;
+    background-color: lightgray;
+    padding: 20px;
+}
+
+.grid-item{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: lightblue;
+    font-size: 20px;
+    font-weight: bold;
+}
+
+
