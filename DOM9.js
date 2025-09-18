a92d4872648dde79b6248ea6c3d6f64727ff243b
yukyk <yusufkhambaty1@gmail.com>
2025-09-18 18:05:04 +0530

    CSS BOX MODAL

---
+++ DOM9.js
@@ -0,0 +1,19 @@
+/* Box 1: Borderless box */
+.box-1{
+    margin: 20px;
+    padding: 15px;
+    background-color: gray;
+
+}
+
+
+/* Box 2: Box with border and box-sizing */
+
+.box-2{
+    margin: 20px;
+    padding: 15px;
+    border: 2px solid black;
+    box-sizing: border-box;
+    background-color: lightblue;
+}
+
