6b6c4aa0b9828c9c4fcc2c766229fa6e3c03c62e
yukyk <yusufkhambaty1@gmail.com>
2025-09-19 16:55:21 +0530

    Delete from Local Storage

---
+++ DOM12.js
@@ -0,0 +1,34 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const phone = event.target.phone.value
+
+    const obj = {
+        username,email, phone
+    }
+    const stingObj = JSON.stringify(obj);
+    localStorage.setItem(email, stingObj);
+
+    displayUser(obj)
+};
+
+function displayUser(obj) {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+        localStorage.removeItem(obj.email);
+    })
+
+    li.textContent = obj.username + "-" + obj.email + "-" + obj.phone;
+    li.appendChild(deleteBtn);
+    
+
+    ul.appendChild(li)
+}
+
+module.exports = handleFormSubmit;
