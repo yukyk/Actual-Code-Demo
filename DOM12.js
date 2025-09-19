416d37181e07a88023f8aeb9f4b972ac1b87d355
yukyk <yusufkhambaty1@gmail.com>
2025-09-19 17:52:13 +0530

    New Chnage

--- DOM12.js
+++ DOM12.js
@@ -32,3 +32,31 @@ function displayUser(obj) {
 }
 
 module.exports = handleFormSubmit;
+
+
+
+
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const username = event.target.username.value
+    const email = event.target.email.value
+    const phone = event.target.phone.value
+
+    const obj = {
+        username,email,phone
+    }
+    const stingObj = JSON.stringify(obj);
+    localStorage.setItem(email, stingObj);
+    displayUser(obj)
+
+}
+function displayUser(obj) {
+    const ul = document.querySelector("ul");
+    const li = document.createElement("li");
+
+    li.textContent = obj.username + "-" + obj.email + "-" + obj.phone;
+    ul.appendChild(li)
+}
+
+module.exports = handleFormSubmit;
