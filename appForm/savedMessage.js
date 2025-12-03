a4065122357ad21dd20fa8dd40f827f66c53ffa0
yukyk <yusufkhambaty1@gmail.com>
2025-12-03 21:14:05 +0530

    task 2

---
+++ appForm/savedMessage.js
@@ -0,0 +1,81 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+      let savedMessage = "";
+    try {
+        savedMessage = fs.readFileSync("user.txt",);
+    } catch (err) {
+        savedMessage = "";
+    }
+
+ 
+    if (url === '/' && method === 'GET') {
+        res.setHeader('Content-Type', 'text/html');
+
+        res.end(`
+            <p style="font-size:20px; color:red;">${savedMessage || "No message saved yet."}</p>
+            <hr>
+            <form action="/submit" method="POST">
+                <label>Enter a message:</label>
+                <input type="text" name="username" />
+                <button type="submit">Submit</button>
+            </form>
+        `);
+    }
+
+    else if (url === '/submit' && method === 'POST') {
+        let body = [];
+
+
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+
+  
+        req.on('end', () => {
+          
+            const combinedBuffer = Buffer.concat(body).toString();
+            const username = combinedBuffer.split('=')[1];
+
+
+            const dataBuffer = Buffer.from(username, 'utf-8');
+
+            fs.writeFile('user.txt', dataBuffer, (err) => {
+                if (err) {
+                    return res.end("Error writing file");
+                }
+
+                res.setHeader('Content-Type', 'text/html');
+                res.end(`<h1>Username saved: ${username}</h1>`);
+            });
+        });
+    }
+
+
+    else if (url === '/read') {
+        fs.readFile('user.txt', (err, data) => {
+            if (err) {
+                res.end("No file found!");
+                return;
+            }
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<h1>Saved User: ${data.toString()}</h1>`);
+        });
+    }
+
+    // 404
+    else {
+        res.statusCode = 404;
+        console.log(' ')
+        res.end("Page Not Found");  
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
