05e37d1ceda6e0f9d6cd974bfa9e02036f8aa907
yukyk <yusufkhambaty1@gmail.com>
2025-12-22 20:29:43 +0530

    The insert update and delete are in the same file

--- SQL/Student Managment API/Controller/studentController.js
+++ SQL/Student Managment API/Controller/studentController.js
@@ -1,23 +1,21 @@
 const db = require('../Utils/util');
-
-const addStudent = (req,res) =>{
-    const { name, email, age } = req.body;
-    const insertQuery = 'INSERT INTO student (name, email,age) VALUES (?, ?,?)'; 
-        console.log('[INSERT] Student:', { name, email, age });
+const studentModel = require('../models/studentTables');
 
 
-    db.execute(insertQuery, [name, email,age], (err, results) => {
-        if (err) {
-            console.log(err.message);
-            res.status(500).send("Error adding entry to the database.");
-            db.end();
-            return;
-        }
-        console.log("Entry added successfully.");
-        res.status(200).send(`Student with name ${name} and successfully added.`) 
-    })
 
-}
+
+const addStudent = async (req, res) =>{
+    try{
+        const {name, email} = req.body;
+        const student = await studentModel.create({
+            name:name, 
+            email:email
+        });
+        res.status(201).send(`Student with name ${name} successfully added.`)
+    } catch(err){
+        res.status(500).send("Error adding entry to the database.");  
+    }
+} 
 
 const getAllStudents = (req, res) => {
     const query = 'SELECT * FROM student';
@@ -48,48 +46,49 @@ const getStudentById = (req, res) => {
     });
 };
 
-const updateStudent = (req,res) => {
+const updateStudent = async(req,res) => {
+    try{
     const { id } = req.params;
     const { name,email,age} = req.body;
-    const updateQuery = 'UPDATE student SET name = ?, email = ?, age = ? WHERE id = ?';
+    const student = await studentModel.findByPk(id);
+    if(!student){
+        res.status(404).send(`Student with id ${id} not found.`);
+    }
+    student.name = name; 
+    student.email = email;
+    await student.save();
+    res.status(200).send(`Student with id ${id} successfully updated.`);
 
-    db.execute(updateQuery, [name, email, age, id], (err,results) => {
-        if(err){
-            console.log(err.message);
-            res.status(500).send("Error updating entry in the database.");
-            db.end();
-            return;
-        }
+    }catch(err){
+        res.status(500).send("Error updating entry in the database.");
 
-        if(results.affectedRows === 0){
-            res.status(404).send(`Student with id ${id} not found.`);
-            return;
-        }
-        res.status(200).send(`Student with id ${id} successfully updated.`);    
-
-
-    });
+    }
+    
 }
 
 
- const deleteStudent = (req,res) => {
-    const { id } = req.params;
-    const deleteQuery = 'DELETE FROM student WHERE id = ?';
-
-    db.execute(deleteQuery, [id], (err,results) => {
-        if(err){
-            console.log(err.message);
-            res.status(500).send("Error deleting entry in the database.");
-            db.end();
-            return;
-        }
-
-        if(results.affectedRows === 0){
+ const deleteStudent = async (req,res) => {
+    try{
+        const { id } = req.params;
+        const student = await studentModel.destroy({
+            where: {
+                id:id 
+            }
+        })
+        if(!student){
             res.status(404).send(`Student with id ${id} not found.`);
-            return;
         }
-        res.status(200).send(`Student with id ${id} successfully deleted.`);    
-    });
+        res.status(200).send(`Student with id ${id} successfully deleted.`);
+
+    } catch(err){
+        console.log(err.message);
+        res.send("Error deleting entry in the database.");
+
+    }
+
+
+
+
 }
 
 module.exports = {
